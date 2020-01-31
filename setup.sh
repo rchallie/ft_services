656c4a9a74af167312166933030c2a24324b0334
Romain Challier <rchallie@student.42.fr>
2020-01-31 19:00:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,13 @@ echo -ne 	"\n\n\033[1;36m
 ██║        ██║███████╗███████║███████╗██║  ██║ ╚████╔╝ ██║╚██████╗███████╗███████║
 ╚═╝        ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝ ╚═════╝╚══════╝╚══════╝
                                                                                   \n\n"
-if brew &> /dev/null
-then
-	echo -ne "\033[1;31m+> Error... Please install brew."
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo -ne "\033[1;31m+>\033[0;33m Intall brew... \n"
+    rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc &> /dev/null
 else
-	echo -ne "\033[1;32m+>\033[0;33m Brew already installed.\n"
+	echo -ne "\033[1;32m+>\033[0;33m Update brew... ! \n"
+    brew update
 fi
 
 echo -ne "\033[1;32m+>\033[0;33m Link folder to goinfre ...\n"
@@ -24,7 +26,7 @@ rm -rf /goinfre/$USER/.minikube
 if minikube &> /dev/null
 then
 	echo -ne "\033[1;33m+>\033[0;33m Minikube check for update ... \n"
-	if brew install minikube &> /dev/null
+	if brew install minikube
 	then
 		echo -ne "\033[1;32m+>\033[0;33m Minikube updated ! \n"
 	else
@@ -43,9 +45,9 @@ fi
 
 # cp research.md $MINIKUBE_HOME/.minikube/files
 echo -ne "\033[1;33m+>\033[0;33m Enable addons ...\n"
-minikube addons enable ingress
+minikube addons enable ingress 
 
-echo -ne "\033[1;32m+>\033[0;33m Start minikube (can take some minutes) ... \n"
+# echo -ne "\033[1;32m+>\033[0;33m Start minikube (can take some minutes) ... \n"
 minikube start --vm-driver=virtualbox
 
 # echo -ne "\033[1;35m+>\033[0;33m Docker : NGINX :\n"
@@ -66,166 +68,17 @@ echo -ne "\033[1;32m+>\033[0;33m Stop minikube ... \n"
 minikube stop
 
 echo -ne "\033[1;32m+>\033[0;33m Copy utils files ... \n"
-cp -avR srcs $MINIKUBE_HOME/.minikube/files &> /dev/null
+cp -avR srcs/containers $MINIKUBE_HOME/.minikube/files/srcs &> /dev/null
 
 # echo -ne "\033[1;32m+>\033[0;33m Restart minikube (can take some minutes) ... \n"
 minikube start --vm-driver=virtualbox
 
 minikube ssh 'docker build -t services/nginx /srcs/nginx/'
 
-# nginx_ip=`minikube service nginx --url`
-# echo -ne "\033[1;33m+>\033[0;33m Nginx IP : $nginx_ip \n"
+kubectl apply -f srcs/yaml/nginx.yaml
+server_ip=`minikube ip`
+echo -ne "\033[1;33m+>\033[0;33m IP : $server_ip \n"
 
+echo -ne "\033[1;32m+>\033[0;33m Open website ... \n"
+open http://$server_ip
 # Remplacer les cmd run et expose par un .yaml Pour multi port
-
-----------------------------
-
-user                            www;
-worker_processes                auto; # it will be determinate automatically by the number of core
-
-error_log                       /var/log/nginx/error.log warn;
-pid                             /var/run/nginx/nginx.pid; # it permit you to use /etc/init.d/nginx reload|restart|stop|start
-
-events {
-    worker_connections          1024;
-}
-
-http {
-    include                     /etc/nginx/mime.types;
-    default_type                application/octet-stream;
-    sendfile                    on;
-    access_log                  /var/log/nginx/access.log;
-    keepalive_timeout           3000;
-    # server {
-    #     listen                  80;
-    #     root                    /www;
-    #     index                   index.html index.htm;
-    #     server_name             localhost;
-    #     client_max_body_size    32m;
-    #     error_page              500 502 503 504  /50x.html;
-    #     location = /50x.html {
-    #           root              /var/lib/nginx/html;
-    #     }
-    # }
-
-    server {
-        listen 80;
-        listen [::]:80;
-
-        server_name www;
-
-        root /www;
-        index index.html;
-
-        location / {
-            autoindex on;
-            try_files $uri $uri/ =404;
-        }
-    }
-
-    server{
-
-        listen 443 ssl ;
-        listen [::]:443 ssl ;
-
-        server_name www;
-
-        ssl_certificate /etc/nginx/ssl/www.pem;
-        ssl_certificate_key /etc/nginx/ssl/www.key;
-
-        root /www;
-        index index.html;
-
-        location / {
-            autoindex on;
-            try_files $uri $uri/ =404;
-        }
-    }
-}
-
-------------------------------------
-
-apiVersion: v1
-kind: Service
-metadata:
-  name: nginx-svc
-  labels:
-    app: myapp
-spec:
-  type: NodePort
-  ports:
-  - port: 80
-    protocol: TCP
-    name: http
-  - port: 443
-    protocol: TCP
-    name: https
-  selector:
-    app: myapp
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: nginx-deployment
-  labels:
-    app: myapp
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: myapp
-  template:
-    metadata:
-      labels:
-        app: myapp
-    spec:
-      containers:
-      - name: nginx
-        image: services/nginx
-        ports:
-        - containerPort: 80
-        - containerPort: 443
-        imagePullPolicy: Never
----
-apiVersion: networking.k8s.io/v1beta1
-kind: Ingress
-metadata:
-  name: nginx-ingress
-  annotations:
-    kubernetes.io/ingress.class: nginx
-    nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
-  namespace: default
-spec:
-  rules:
-  - host: ftservices.rchallie
-  - http:
-      paths:
-      - path: /
-        backend:
-          serviceName: nginx-svc
-          servicePort: 80
-
------------------------------------------------------------------
-
-FROM alpine:latest
-
-# update && install nginx && bash && openssl && folder for nginx pid
-RUN apk update && apk add nginx \
-&& apk add --no-cache --upgrade bash \
-&& apk add openssl && mkdir -p var/run/nginx
-
-# create user for nginx && generate nginx folder && add permissions
-RUN adduser -D -g 'www' www \
-&& mkdir /www && chown -R www:www /var/lib/nginx \
-&& chown -R www:www /www
-
-# SSL
-RUN mkdir /etc/nginx/ssl \
-&& openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/www.pem -keyout /etc/nginx/ssl/www.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=rchallie/CN=ft_services"
-
-# Nginx config
-RUN rm /etc/nginx/nginx.conf
-COPY ./nginx.conf /etc/nginx/nginx.conf
-
-# Start nginx
-CMD [ "nginx" , "-g", "daemon off;" ]
